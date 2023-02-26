4621b1bc194c2cf470c66d00df6b2c2cc4bfebd9
aristidelauga <aristide.lauga@gmail.com>
2023-02-26 17:37:00 +0100

    Initial Commit

---
+++ Beers/BeersApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeersApp.swift
+//  Beers
+//
+//  Created by Aristide LAUGA on 26/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct BeersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
